a1ef99645c198c93e166d4b06d2a881a7d0b3e57
Matheus3007 <matheusrp2009@gmail.com>
2025-10-12 20:19:00 -0300

    fix back

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+import express from 'express';
+import cors from 'cors';
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
